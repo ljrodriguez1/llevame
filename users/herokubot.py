5e95c33408c36aaf4491d47f8d81f1c1e3ea21cc
Lucas <ljrodriguez1@uc.cl>
2019-06-10 16:26:28 -0400

    fixed settings

--- users/herokubot.py
+++ users/herokubot.py
@@ -1,5 +1,10 @@
-import logging
 import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myapp.settings')
+
+import logging
 
 from telegram import (ReplyKeyboardMarkup, ReplyKeyboardRemove)
 from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters, RegexHandler,
@@ -8,11 +13,6 @@ from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters, Rege
 from models import Usuario
 AGE, GENDER, PHOTO, LOCATION, BIO = range(5)
 
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myapp.settings')
 
 def start(update, context):
     if update.message.chat.id in [x.chat_id for x in Usuario.objects.all()]:
