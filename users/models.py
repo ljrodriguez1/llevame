53ddd672cbccbf0a594c8192560cf33efd276173
Lucas <ljrodriguez1@uc.cl>
2019-06-10 16:22:42 -0400

    Created class user

--- users/models.py
+++ users/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import AbstractUser
+#from django.contrib.auth.models import AbstractUser
 from django.db import models
 from math import sin, cos, sqrt, atan2, radians
 
