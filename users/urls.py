72b31a8bbb77eff822def6e330461797785ee9a5
Lucas <ljrodriguez1@uc.cl>
2019-06-10 20:06:03 -0400

    Making user, abstract user

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,10 @@
 # users/urls.py
 from django.urls import path
-from . import views
+#from . import views
+
 
 urlpatterns = [
+    """
     path('signup/', views.SignUp.as_view(), name='signup'),
     path('<int:pk>/ubicaciones/', views.UbicacionIndex.as_view(), name='ubicacion'),
     path('<int:pk>/newubicacion/', views.newubicacion, name='newubicacion'),
@@ -11,4 +13,5 @@ urlpatterns = [
     path('<int:pk>/conductor/', views.conductor, name='conductor'),
     path('<int:pk>/newllevame/', views.newllevame, name='newllevame'),
     path('<int:pk>/nollevar/', views.nollevar, name='nollevar'),
+    """
 ]
