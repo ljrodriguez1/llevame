fb735d550d0549750910bbf8f0f192b3f992eaf1
Lucas <ljrodriguez1@uc.cl>
2019-06-12 14:21:28 -0400

    minor

--- herokubot.py
+++ herokubot.py
@@ -96,7 +96,7 @@ def destino(update, context):
 def accept(update, context):
     opcion = update.message.text
     user = Usuario.objects.get(pk=update.effective_user.id)
-    if user.maneja:
+    if user.manejo:
         update.message.reply_text("Tu viaje sera a las {}".format(opcion),
             reply_markup=ReplyKeyboardRemove())
     else:
