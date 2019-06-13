7bf8d810b7ede59fa18d6b77300efa0645c54923
Lucas <ljrodriguez1@uc.cl>
2019-06-13 16:41:46 -0400

    minor

--- users/models.py
+++ users/models.py
@@ -104,7 +104,7 @@ class Usuario(AbstractUser):
              self.buscandoviaje.delete()
         except:
             pass
-        viaje = BuscandoViaje(conductor=self, hora=hora, ida=tramo, dia=dia)
+        viaje = BuscandoViaje(user=self, hora=hora, ida=tramo, dia=dia)
         viaje.save()
         self.save()
     
